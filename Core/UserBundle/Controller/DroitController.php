4af6d42c4f8d6aea921211e9d3c1e6c3a3fcec4f
nicolas <pin.nicolas@free.fr>
2015-09-02 21:23:25 +0200

    Remise en place de l'action update qui avait été supprimé par erreur

--- Core/UserBundle/Controller/DroitController.php
+++ Core/UserBundle/Controller/DroitController.php
@@ -105,4 +105,34 @@ class DroitController extends Controller
         return array(array_merge($updatedDroits, $nouveauxDroits), $modified);
     }
 
+    /**
+     * Edits an existing Droit entity.
+     *
+     */
+    public function updateAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('UserBundle:Droit');
+
+        if ($request->getMethod() == 'POST') {
+            $post = $request->request->get('droits');
+            foreach ($post AS $groupe_id => $groupe) {
+                foreach ($groupe AS $bundle_name => $values) {
+                    $droit = $entity->findOneBy(array('groupe' => $groupe_id, 'bundle' => $bundle_name));
+                    $droits = $droit->getDroits();
+                    foreach ($droits AS $tag2 => $child) {
+                        foreach ($child AS $tag3 => $value) {
+                            $droits[$tag2][$tag3] = isset($post[$groupe_id][$bundle_name][$tag2][$tag3]) ? true : false;
+                        }
+                    }
+                    $droit->setDroits($droits);
+                    $em->persist($droit);
+                    $em->flush();
+                }
+            }
+        }
+
+        return $this->redirect($this->generateUrl('superadmin_droits'));
+    }
 }
